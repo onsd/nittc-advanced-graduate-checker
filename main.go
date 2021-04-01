ed25537e7f51e2b40619e006b6fe7ce543abd3b7
Takamichi Omori <merrytakamerry@gmail.com>
2021-04-01 15:51:15 +0900

    show as table

--- main.go
+++ main.go
@@ -39,87 +39,126 @@ func main() {
 		syllabuses[t] = syllabus
 	}
 
-	for key, syllabus := range syllabuses {
-		fmt.Printf("--- %s:\n%v\n\n", key, syllabus)
+	for _, syllabus := range syllabuses["専門科目"] {
+		fmt.Printf("--- %s:\n%v\n\n", syllabus.Group, syllabus)
 	}
 
+
+	table := createTable(syllabuses["専門科目"])
 	app := tview.NewApplication()
-	table := tview.NewTable().
-		SetFixed(1, 1).SetSelectable(true, false)
-
-	syllabus := syllabuses["専門科目"]
-	table.SetCell(0, 0,
-		tview.NewTableCell("Name").
-			SetTextColor(tcell.ColorYellow).
-			SetAlign(tview.AlignCenter),
-	)
-	table.SetCell(0, 1,
-		tview.NewTableCell("Credits").
-			SetTextColor(tcell.ColorYellow).
-			SetAlign(tview.AlignCenter),
-	)
-	table.SetCell(0, 2,
-		tview.NewTableCell("Require ").
-			SetTextColor(tcell.ColorYellow).
-			SetAlign(tview.AlignCenter),
-	)
-	table.SetCell(0, 3,
-		tview.NewTableCell("JABEE Required").
-			SetTextColor(tcell.ColorYellow).
-			SetAlign(tview.AlignCenter),
-	)
-	table.SetCell(0, 4,
-		tview.NewTableCell("Earned Credit").
-			SetTextColor(tcell.ColorYellow).
-			SetAlign(tview.AlignCenter),
-	)
-	for i, subject := range syllabus[0].Subjects {
-		table.SetCell(i+1, 0,
-			tview.NewTableCell(subject.Name).
-				SetTextColor(tcell.ColorWhite).
-				SetAlign(tview.AlignCenter),
-		)
-		table.SetCell(i+1, 1,
-			tview.NewTableCell(strconv.Itoa(subject.Credits)).
-				SetTextColor(tcell.ColorWhite).
-				SetAlign(tview.AlignCenter),
-		)
-		table.SetCell(i+1, 2,
-			tview.NewTableCell(strconv.FormatBool(subject.Required)).
-				SetTextColor(tcell.ColorWhite).
-				SetAlign(tview.AlignCenter),
-		)
-		table.SetCell(i+1, 3,
-			tview.NewTableCell(strconv.FormatBool(subject.JABEE)).
-				SetTextColor(tcell.ColorWhite).
-				SetAlign(tview.AlignCenter),
-		)
-		table.SetCell(i+1, 4,
-			tview.NewTableCell(strconv.FormatBool(subject.EarnCredit)).
-				SetTextColor(tcell.ColorWhite).
-				SetAlign(tview.AlignCenter),
-		)
-	}
-	table.Select(0, 0).SetFixed(1, 1).SetDoneFunc(func(key tcell.Key) {
-		if key == tcell.KeyEscape {
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyEsc {
 			app.Stop()
+			return nil
 		}
-		if key == tcell.KeyEnter {
-			table.SetSelectable(true, true)
+		if event.Key() == tcell.KeyCtrlC {
+			app.Stop()
+			return nil
 		}
-	}).SetSelectedFunc(func(row int, column int) {
-		if syllabus[0].Subjects[row-1].EarnCredit {
-			table.GetCell(row, 4).SetTextColor(tcell.ColorWhite).SetText("false")
-
-		} else {
-			table.GetCell(row, 4).SetTextColor(tcell.ColorRed).SetText("true")
-
-		}
-		syllabus[0].Subjects[row-1].EarnCredit = !syllabus[0].Subjects[row-1].EarnCredit
-		table.SetSelectable(true, false)
+		return event
 	})
 	if err := app.SetRoot(table, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
 	}
+
+	fmt.Println("success fully finished")
+}
+
+func createTable(syllabuses []Syllabus) tview.Primitive{
+	var tables  []*tview.Table
+	for _, syllabus := range syllabuses {
+		table := tview.NewTable().
+			SetFixed(1, 1).
+			SetSelectable(true, false)
+		table.SetCell(0, 0,
+			tview.NewTableCell("Name").
+				SetTextColor(tcell.ColorYellow).
+				SetAlign(tview.AlignCenter),
+		)
+		table.SetCell(0, 1,
+			tview.NewTableCell("Credits").
+				SetTextColor(tcell.ColorYellow).
+				SetAlign(tview.AlignCenter),
+		)
+		table.SetCell(0, 2,
+			tview.NewTableCell("Require ").
+				SetTextColor(tcell.ColorYellow).
+				SetAlign(tview.AlignCenter),
+		)
+		table.SetCell(0, 3,
+			tview.NewTableCell("JABEE Required").
+				SetTextColor(tcell.ColorYellow).
+				SetAlign(tview.AlignCenter),
+		)
+		table.SetCell(0, 4,
+			tview.NewTableCell("Earned Credit").
+				SetTextColor(tcell.ColorYellow).
+				SetAlign(tview.AlignCenter),
+		)
+		for i, subject := range syllabus.Subjects {
+			table.SetCell(i+1, 0,
+				tview.NewTableCell(subject.Name).
+					SetTextColor(tcell.ColorWhite).
+					SetAlign(tview.AlignCenter),
+			)
+			table.SetCell(i+1, 1,
+				tview.NewTableCell(strconv.Itoa(subject.Credits)).
+					SetTextColor(tcell.ColorWhite).
+					SetAlign(tview.AlignCenter),
+			)
+			table.SetCell(i+1, 2,
+				tview.NewTableCell(strconv.FormatBool(subject.Required)).
+					SetTextColor(tcell.ColorWhite).
+					SetAlign(tview.AlignCenter),
+			)
+			table.SetCell(i+1, 3,
+				tview.NewTableCell(strconv.FormatBool(subject.JABEE)).
+					SetTextColor(tcell.ColorWhite).
+					SetAlign(tview.AlignCenter),
+			)
+			table.SetCell(i+1, 4,
+				tview.NewTableCell(strconv.FormatBool(subject.EarnCredit)).
+					SetTextColor(tcell.ColorWhite).
+					SetAlign(tview.AlignCenter),
+			)
+		}
+		table.Select(0, 0).SetFixed(1, 1).SetDoneFunc(func(key tcell.Key) {
+			if key == tcell.KeyEnter {
+				table.SetSelectable(true, true)
+			}
+		}).SetSelectedFunc(func(row int, column int) {
+			if syllabus.Subjects[row-1].EarnCredit {
+				table.GetCell(row, 4).SetTextColor(tcell.ColorWhite).SetText("false")
+
+			} else {
+				table.GetCell(row, 4).SetTextColor(tcell.ColorRed).SetText("true")
+
+			}
+			syllabus.Subjects[row-1].EarnCredit = !syllabus.Subjects[row-1].EarnCredit
+			table.SetSelectable(true, false)
+		})
+		table.
+			SetBorder(true).
+			SetTitle(syllabus.Group)
+		tables = append(tables, table)
+	}
+
+	flex := tview.NewFlex()
+	var rows []*tview.Flex
+	row := tview.NewFlex().SetDirection(tview.FlexRow)
+	for i, table := range tables {
+		row.AddItem(table, 0, 1, false)
+		if i % 3 == 2 {
+			rows = append(rows, row)
+			row = tview.NewFlex().SetDirection(tview.FlexRow)
+		}
+	}
+	fmt.Println(rows)
+
+	for _, row := range rows {
+		flex.AddItem(row, 0, 2, true)
+	}
+
+	return 	flex
 }
 
